5c7f1841fe14c6ad5cc18cbe43898802f827c343
Matt Gray <matt.gray@7digital.com>
2015-02-10 17:31:31 +0000

    read data from tcp

--- unikernel.ml
+++ unikernel.ml
@@ -8,9 +8,11 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
     S.listen_tcpv4 s ~port:4444 (fun flow ->
         let dst, dst_port = S.TCPV4.get_dest flow in
         let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
-        C.log_s c message
-        >>= fun () ->
-        S.TCPV4.close flow
+        C.log c message;
+        S.TCPV4.read flow >>= function
+        | `Ok b -> C.log_s c ( "read some data: " ^ (Cstruct.to_string b) ) >>= fun () -> S.TCPV4.close flow
+        | `Eof -> C.log_s c "read: eof"
+        | `Error _ -> C.log_s c "read: error"
       );
     S.listen s
 end
