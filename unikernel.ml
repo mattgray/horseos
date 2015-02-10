9fc1936acc745417f0c762655aefb358b4ff07fe
Matt Gray <matt.gray@7digital.com>
2015-02-10 17:09:30 +0000

    understanding let binding and printf now

--- unikernel.ml
+++ unikernel.ml
@@ -7,7 +7,8 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
     (* start listening on tcp:4444 (randomly chosen nice port) *)
     S.listen_tcpv4 s ~port:4444 (fun flow ->
         let dst, dst_port = S.TCPV4.get_dest flow in
-        C.log_s c "Got a connection"
+        let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
+        C.log_s c message
         >>= fun () ->
         S.TCPV4.close flow
       );
