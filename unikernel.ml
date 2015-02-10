6f35ef4d0dda3e0928380366a53dd9e7f9c72cce
Matt Gray <matt.gray@7digital.com>
2015-02-10 17:04:47 +0000

    listen on a port

--- unikernel.ml
+++ unikernel.ml
@@ -4,6 +4,12 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
 
   let start c s = 
     C.log c "HorseOS is starting.";
-    C.log c "HorseOS is exiting.";
-    Lwt.return ()
+    (* start listening on tcp:4444 (randomly chosen nice port) *)
+    S.listen_tcpv4 s ~port:4444 (fun flow ->
+        let dst, dst_port = S.TCPV4.get_dest flow in
+        C.log_s c "Got a connection"
+        >>= fun () ->
+        S.TCPV4.close flow
+      );
+    S.listen s
 end
