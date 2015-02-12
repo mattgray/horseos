52b4bd5cfd19685a6707563b7166f27d76c00c27
Matt Gray <matt.gray@7digital.com>
2015-02-12 09:25:05 +0000

    refactor so i can understand better how >> and  >>= work

--- unikernel.ml
+++ unikernel.ml
@@ -2,17 +2,26 @@ open Lwt
 
 module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
 
+
+  let callback c flow =
+  
+    let log_conn c flow =
+      let dst, dst_port = S.TCPV4.get_dest flow in
+      let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
+      C.log_s c message in
+
+    let handle_read = function
+      | `Ok buf -> C.log_s c ( "read some data: " ^ (Cstruct.to_string buf) )
+      | `Eof -> C.log_s c "read: eof"
+      | `Error _ -> C.log_s c "read: error" in
+
+    log_conn c flow >> 
+    S.TCPV4.read flow >>=
+    handle_read >>
+    S.TCPV4.close flow
+
   let start c s = 
     C.log c "HorseOS is starting.";
-    (* start listening on tcp:4444 (randomly chosen nice port) *)
-    S.listen_tcpv4 s ~port:4444 (fun flow ->
-        let dst, dst_port = S.TCPV4.get_dest flow in
-        let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
-        C.log c message;
-        S.TCPV4.read flow >>= function
-        | `Ok b -> C.log_s c ( "read some data: " ^ (Cstruct.to_string b) ) >>= fun () -> S.TCPV4.close flow
-        | `Eof -> C.log_s c "read: eof"
-        | `Error _ -> C.log_s c "read: error"
-      );
+    S.listen_tcpv4 s ~port:4444 (callback c);
     S.listen s
 end
