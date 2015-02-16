545e66a5bb68fc6c410e2a92b5d542e3252620d9
Matt Gray <matt.gray@7digital.com>
2015-02-16 09:00:16 +0000

    horseos connections can talk to each other

--- unikernel.ml
+++ unikernel.ml
@@ -10,24 +10,33 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
       let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
       C.log_s c message in
 
-    let rec handle_read flow =
-      let handle_write buf = S.TCPV4.write flow buf >>= function
-        | `Ok () -> C.log_s c "write" >> handle_read flow
-        | `Eof -> C.log_s c "write: eof" >> S.TCPV4.close flow
-        | `Error _ -> C.log_s c "write: error" >> S.TCPV4.close flow in
-      S.TCPV4.read flow >>= function
+    let messages = Lwt_condition.create () in
+
+    let rec read_input flow =
+      S.TCPV4.read flow >>=function
         | `Eof -> C.log_s c "read: eof" >> S.TCPV4.close flow
         | `Error _ -> C.log_s c "read: error" >> S.TCPV4.close flow
-        | `Ok buf -> C.log_s c ( "read: " ^ Cstruct.to_string buf) >> handle_write buf in
+        | `Ok buf -> return ( Lwt_condition.broadcast messages buf ) >> read_input flow in
 
-    let write_welcome flow = 
-      let welcome_message = Cstruct.of_string horse_ascii in
-      S.TCPV4.write flow welcome_message >>= function
-        | `Ok () -> C.log_s c "write" >> handle_read flow
+    let rec handle_message flow = Lwt_condition.wait messages >>=
+      fun message -> S.TCPV4.write flow message  >>= function
+        | `Ok () -> C.log_s c "write" >> handle_message flow
         | `Eof -> C.log_s c "write: eof" >> S.TCPV4.close flow
         | `Error _ -> C.log_s c "write: error" >> S.TCPV4.close flow in
 
-      C.log c "HorseOS is starting.";
-      S.listen_tcpv4 s ~port:4444 (fun flow -> log_conn flow >> write_welcome flow);
-      S.listen s
+    let write_welcome flow =
+      let welcome_message = Cstruct.of_string horse_ascii in
+      S.TCPV4.write flow welcome_message >>= function
+        | `Ok () -> C.log_s c "write" >> return ()
+        | `Eof -> C.log_s c "write: eof" >> S.TCPV4.close flow
+        | `Error _ -> C.log_s c "write: error" >> S.TCPV4.close flow in
+
+    C.log c "HorseOS is starting.";
+
+    S.listen_tcpv4 s ~port:4444 (fun flow ->
+      log_conn flow >> write_welcome flow >>
+      join [ read_input flow; handle_message flow]
+    );
+
+    S.listen s;
 end
