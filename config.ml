e4927f90dd767da16e4abbfe95aeaa2a78e095c5
Matt Gray <matt.gray@7digital.com>
2015-03-04 22:05:26 +0000

    building horseos for xen is now possible

--- config.ml
+++ config.ml
@@ -2,7 +2,17 @@ open Mirage
 
 let main = foreign "Unikernel.Main" (console @-> stackv4 @-> job)
 
-let stackv4 = socket_stackv4 default_console [Ipaddr.V4.any]
+let xen =
+  try match Sys.getenv "XEN" with
+    | "" -> false
+    | _ -> true
+  with Not_found -> false
+
+let stackv4 =
+  if xen then
+    direct_stackv4_with_dhcp default_console tap0
+  else
+    socket_stackv4 default_console [Ipaddr.V4.any]
 
 let () =
   register "horseos" [
