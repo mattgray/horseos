c051632162676cd43c402cbc7e4dddac90df9cb6
Matt Gray <matt.gray@7digital.com>
2015-03-04 21:19:27 +0000

    simplify

--- config.ml
+++ config.ml
@@ -2,9 +2,9 @@ open Mirage
 
 let main = foreign "Unikernel.Main" (console @-> stackv4 @-> job)
 
-let stack console = socket_stackv4 console [Ipaddr.V4.any]
+let stackv4 = socket_stackv4 default_console [Ipaddr.V4.any]
 
 let () =
   register "horseos" [
-      main $ default_console $ stack default_console
+      main $ default_console $ stackv4
   ]
