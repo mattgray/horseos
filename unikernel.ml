049958a42996e46c1813b50042223a97ae6093a9
Matt Gray <matt.gray@7digital.com>
2015-04-03 13:00:20 +0100

    delete code no longer required, add some logging

--- unikernel.ml
+++ unikernel.ml
@@ -52,46 +52,9 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
 
   let users = Hashtbl.create 10
 
-  let clean_buf buf = Cstruct.to_string buf |> String.trim |> String.escaped
-
-  let logger c message = C.log c message
-(*
-  let log_conn log session =
-      let dst, dst_port = S.TCPV4.get_dest session.flow in
-      let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
-      log message; return ()
-
-  let close_conn log session reason =
-      let message = ( Username.to_string session.name ) ^ " left: " ^ reason ^ "\n" in
-      log reason;
-      Hashtbl.remove users ( Username.to_string session.name );
-      Lwt_condition.broadcast messages message;
-      S.TCPV4.close session.flow
-
-  let write close_conn session message =
-    S.TCPV4.write session.flow ( Cstruct.of_string message ) >>= function
-        | `Eof -> close_conn session "write: eof"
-        | `Error _ -> close_conn session "write: error"
-        | `Ok () -> Lwt.return_unit
-
-  let read close_conn session with_message =
-    S.TCPV4.read session.flow >>= function
-      | `Eof -> close_conn session "read: eof"
-      | `Error _ -> close_conn session "read: error"
-      | `Ok buf -> match Cstruct.get_uint8 buf 0, Cstruct.get_uint8 buf 1 with
-        | 255, 244 -> close_conn session "quit"
-        | 255, _ -> return ()
-        | _ -> match clean_buf buf with
-          | "" -> return ()
-          | m -> with_message m
-*)
   let start c s =
-    let logger = logger c in
-  (*  let log_conn = log_conn logger in
-    let close_conn = close_conn logger in
-    let write = write close_conn in
-    let read = read close_conn in
-*)
+    let log message = C.log c message in
+
     let write_welcome session = Session.write session horse_ascii in
 
     let rec listen_input session username =
@@ -114,23 +77,29 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
           >> Session.close session_initial "bad username"
         else
         (
-          let on_close reason = ( Lwt_condition.broadcast messages ( username ^ " has quit (" ^ reason ^  ")\n" );
-                                  Hashtbl.remove users username ) in
+          let on_close reason =
+              let message = ( username ^ " has quit (" ^ reason ^  ")\n" ) in
+                log message;
+                Lwt_condition.broadcast messages ( username ^ " has quit (" ^ reason ^  ")\n" );
+                Hashtbl.remove users username
+              in
           let session = Session.on_close session_initial on_close in
           Hashtbl.add users username session;
-          logger ( username ^ " joined" );
+          log ( username ^ " joined" );
           Lwt_condition.broadcast messages (username ^ " joined\n" );
           write_userinfo session
           >> join [ listen_input session username; relay_messages session ]
         )
       ) in
 
-    logger "HorseOS is starting.";
+    log "HorseOS is starting.";
 
     S.listen_tcpv4 s ~port:4444 (fun flow ->
 
-      (*let session_initial = { flow = flow; name = Username.unknown } in
-      log_conn session_initial*)
+      let dst, dst_port = S.TCPV4.get_dest flow in
+      let message = Printf.sprintf ("Got a connection from %s on port %d") (Ipaddr.V4.to_string dst) dst_port in
+      log message;
+
       let session_initial = Session.of_flow flow in 
       write_welcome session_initial
       >> main session_initial
