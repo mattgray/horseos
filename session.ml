4e7e419ee06e8e245ef466bfa5e9fca546746871
Matt Gray <matt.gray@7digital.com>
2015-04-04 11:48:31 +0100

    expand all the STACKV4 aliases

--- session.ml
+++ session.ml
@@ -1,6 +1,6 @@
 open V1_LWT
 
-type t = { flow: V1_LWT.STACKV4.TCPV4.flow; on_close: ( string -> unit ) }
+type t = { flow: STACKV4.TCPV4.flow; on_close: ( string -> unit ) }
 
 let of_flow f = { flow = f; on_close = fun _ -> () }
 
@@ -8,17 +8,17 @@ let on_close session closer = { session with on_close = closer }
 
 let close session reason =
       session.on_close reason;
-      S.TCPV4.close session.flow
+      STACKV4.TCPV4.close session.flow
 
 let write session message =
-  S.TCPV4.write session.flow ( Cstruct.of_string message ) >>= function
+  STACKV4.TCPV4.write session.flow ( Cstruct.of_string message ) >>= function
     | `Eof -> close session "write: eof"
     | `Error _ -> close session "write: error"
     | `Ok () -> Lwt.return_unit
 
 let read session message_handler =
   let clean_buf buf = Cstruct.to_string buf |> String.trim |> String.escaped in
-    S.TCPV4.read session.flow >>= function
+    STACKV4.TCPV4.read session.flow >>= function
       | `Eof -> close session "read: eof"
       | `Error _ -> close session "read: error"
       | `Ok buf -> match Cstruct.get_uint8 buf 0, Cstruct.get_uint8 buf 1 with
