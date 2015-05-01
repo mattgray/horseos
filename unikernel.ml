bce1b463ac4c060ae024e7539dedc194615a1143
Matt Gray <matt.gray@7digital.com>
2015-05-01 19:11:32 +0100

    Horse_greeter module

--- unikernel.ml
+++ unikernel.ml
@@ -2,10 +2,13 @@ open Lwt
 open Session
 
 module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
-    
-  let horse_ascii = "welcome to HorseOS 0.02          |\\    /|\n                              ___| \\,,/_/\n                           ---__/ \\/    \\\n                          __--/     (D)  \\\n                          _ -/    (_      \\\n                         // /       \\_ / ==\\\n   __-------_____--___--/           / \\_ O o)\n  /                                 /   \\==/`\n /                                 /\n||          )                   \\_/\\\n||         /              _      /  |\n| |      /\\______      ___\\    /\\  :\n| /   __-@@\\_/   ------    |  |   \\ \\\n |   -  -   \\               | |     \\ )\n |  |   -  | \\              | )     | |\n  | |    | |                 | |    | |\n  | |    < |                 | |   |_/\n  < |    /__\\                <  \\\n  /__\\                       /___\\\n\nplease enter a username: "
 
   module Session = Session.Tcp(S)
+
+  module Horse_greeter = struct
+    let horse_ascii = "welcome to HorseOS 0.02          |\\    /|\n                              ___| \\,,/_/\n                           ---__/ \\/    \\\n                          __--/     (D)  \\\n                          _ -/    (_      \\\n                         // /       \\_ / ==\\\n   __-------_____--___--/           / \\_ O o)\n  /                                 /   \\==/`\n /                                 /\n||          )                   \\_/\\\n||         /              _      /  |\n| |      /--______      ___\\    /\\  :\n| /   __-  - _/   ------    |  |   \\ \\\n |   -  -   /                | |     \\ )\n |  |   -  |                 | )     | |\n  | |    | |                 | |    | |\n  | |    < |                 | |   |_/\n  < |    /__\\                <  \\\n  /__\\                       /___\\\n\nplease enter a username: "
+    let greet session = Session.write session horse_ascii
+  end
 
   let messages = Lwt_condition.create ()
 
@@ -13,8 +16,6 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
 
   let start c s =
     let log message = C.log c message in
-
-    let write_welcome session = Session.write session horse_ascii in
 
     let rec listen_input session username =
       let broadcast message = return ( Lwt_condition.broadcast messages ( username ^ ": " ^ message ^ "\n") ) in
@@ -60,7 +61,7 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
       log message;
 
       let session_initial = Session.of_flow flow in 
-      write_welcome session_initial
+      Horse_greeter.greet session_initial
       >> main session_initial
     );
 
