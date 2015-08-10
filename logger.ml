94b3b7576cda57645e44699f7e9b29031dda9fdc
Matt Gray <matt.gray@7digital.com>
2015-08-10 08:42:56 +0100

    indentising

--- logger.ml
+++ logger.ml
@@ -24,17 +24,20 @@ module Log_to_syslog_udp (U: V1_LWT.UDPV4) (CL: V1.CLOCK) = struct
   open CL
   type config = { udp: U.t; ip: Ipaddr.V4.t; port: int }
   type t = { udp: U.t; ip: Ipaddr.V4.t; port: int }
-  let create (config : config) = {udp = config.udp; ip = config.ip; port =
-    config.port  }
+  let create (config: config) = {
+      udp = config.udp;
+      ip = config.ip;
+      port = config.port
+  }
   let to_rfc3339_string (t : CL.tm) =
     sprintf "%04i-%02i-%02iT%02i:%02i:%02iZ"
-    (t.tm_year + 1900) (t.tm_mon + 1) t.tm_mday t.tm_hour t.tm_min t.tm_sec
+      (t.tm_year + 1900) (t.tm_mon + 1) t.tm_mday t.tm_hour t.tm_min t.tm_sec
   let send t time message =
     let syslog_message =
       sprintf "<34>1 %s giddyup.horse horseos - - [TOKEN@41058] %s"
-      (to_rfc3339_string (CL.gmtime time)) message in
-    U.write t.udp
-      ~dest_ip:t.ip ~dest_port:t.port (Cstruct.of_string syslog_message)
+        (to_rfc3339_string (CL.gmtime time)) message in
+    U.write
+      t.udp ~dest_ip:t.ip ~dest_port:t.port (Cstruct.of_string syslog_message)
 end
 
 module Make (CL: V1.CLOCK) = struct
