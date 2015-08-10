f38dae56cac9497d1b89081fb2f7939d04dbf265
Matt Gray <matt.gray@7digital.com>
2015-08-10 09:09:12 +0100

    more better indenterising

--- logger.ml
+++ logger.ml
@@ -22,7 +22,11 @@ end
 
 module Log_to_syslog_udp (U: V1_LWT.UDPV4) (CL: V1.CLOCK) = struct
   open CL
-  type config = { udp: U.t; ip: Ipaddr.V4.t; port: int }
+  type config = {
+    udp: U.t;
+    ip: Ipaddr.V4.t;
+    port: int;
+  }
   type t = { udp: U.t; ip: Ipaddr.V4.t; port: int }
   let create (config: config) = {
       udp = config.udp;
@@ -43,11 +47,13 @@ end
 module Make (CL: V1.CLOCK) = struct
   open CL
 
-  let build_destination (type a) (module L : Log_destination with type config = a) config =
-    (module struct
-      module Log_destination = L
-      let this = L.create config
-    end : Log_destination_instance)
+  let build_destination
+    (type a)
+    (module L : Log_destination with type config = a) config =
+      (module struct
+        module Log_destination = L
+        let this = L.create config
+      end : Log_destination_instance)
 
   let create loggers =
     fun message ->
